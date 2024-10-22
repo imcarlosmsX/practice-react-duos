5ee3b3fc7a0203f26587899ec386a70a4eed02bc
imcarlosmsX <sandovalcarlos@uninorte.edu.co>
2024-10-21 19:37:47 -0500

    Refactor delete button and task display in App component

--- src/App.tsx
+++ src/App.tsx
@@ -15,33 +15,34 @@ const App = () => {
     const newTasks = tasks.filter((_, i) => i !== index);
     setTasks(newTasks);
   };
-
+  
   return (
-      <div className="App">
-      <h1 className="text-3xl text-center mt-5">To Do List</h1>
-        <div className='flex items-center justify-center mt-5'>
-          <div >
-          <input className='mx-5'
-  type="text"
-  value={taskInput}
-  onChange={(e) => setTaskInput(e.target.value)}
-  placeholder="Escribe una tarea"
-/>
-</div>
-<button onClick={addTask}>Añadir</button>
-</div>
-<div className='flex flex-col items-center justify-center mx-5'>
-  {tasks.map((task, index) => (
-    <div key={index} className="task flex items-center justify-between w-full my-2">
-      <span className='mx-5 border border-gray-300 p-2 rounded flex-grow'>{task}</span>
-      <button onClick={() => deleteTask(index)} className="delete-button hover:bg-red-500 p-2 rounded border border-gray-300 flex-shrink-0">
-        <img src="/public/assets/delete-svgrepo-com.svg" alt="" className="w-6 h-6" />
-      </button>
+    <div className="App">
+      <h1 className="text-3xl text-center mt-10 mb-5">To Do List</h1>
+      <div className='flex items-center justify-center mt-5 mb-10'>
+        <div>
+          <input
+            className='mx-5 border border-gray-300 p-2 rounded'
+            type="text"
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+            placeholder="Type the task"
+          />
+        </div>
+        <button onClick={addTask} className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">Add</button>
+      </div>
+      <div className='flex flex-col items-center justify-center mx-5 space-y-4'>
+        {tasks.map((task, index) => (
+          <div key={index} className="task flex items-center justify-between w-full">
+            <span className='mx-5 border border-gray-300 p-2 rounded flex-grow'>{task}</span>
+            <button onClick={() => deleteTask(index)} className="delete-button hover:bg-red-500 p-2 rounded border border-gray-300 flex-shrink-0">
+              <img src="/public/assets/delete-svgrepo-com.svg" alt="" className="w-6 h-6" />
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  ))}
-</div>
-</div>
-);
-};
-
-export default App;
+  );
+  };
+  
+  export default App;
