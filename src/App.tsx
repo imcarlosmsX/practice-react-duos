1c70afbf303e40f9f44d59acd7764e2b6c52076c
jesusdlacrz <delacruzjd@uninorte.edu.co>
2024-10-21 19:20:46 -0500

    xd

--- src/App.tsx
+++ src/App.tsx
@@ -17,20 +17,23 @@ const App = () => {
   };
 
   return (
-    <div className="App">
-      <div>
-        <input
-          type="text"
-          value={taskInput}
-          onChange={(e) => setTaskInput(e.target.value)}
-          placeholder="Escribe una tarea"
-        />
+      <div className="App">
+      <h1 className="text-3xl text-center mt-5">To Do List</h1>
+        <div className='flex items-center justify-center mt-5'>
+          <div >
+          <input className='mx-5'
+            type="text"
+            value={taskInput}
+            onChange={(e) => setTaskInput(e.target.value)}
+            placeholder="Escribe una tarea"
+          />
+          </div>
         <button onClick={addTask}>Añadir</button>
       </div>
-      <div>
+      <div className='flex items-center justify-center mx-5'>
         {tasks.map((task, index) => (
           <div key={index} className="task">
-            <span>{task}</span>
+            <span className='mx-5'>{task}</span>
             <button onClick={() => deleteTask(index)} className="delete-button">
   <img src="/public/assets/delete-svgrepo-com.svg" alt="" className="w-5" /></button>
           </div>
